6dba8795b234724ea39728e76a4a55123d34ba65
Kaja Grabowska <kaja.d.grabowska@gmail.com>
2021-10-13 12:43:03 -0500

    Added code for displaying the relationship between Programs and Events on the Front-End.

--- app/public/wp-content/themes/fictional-university-theme/front-page.php
+++ app/public/wp-content/themes/fictional-university-theme/front-page.php
@@ -38,9 +38,9 @@
         <div class="event-summary">
           <a class="event-summary__date t-center" href="#">
             <span class="event-summary__month"><?php
-              $eventDate = new DateTime(get_field('event_date', false, false));
-              echo $eventDate->format('M')
-              ?></span>
+                                                $eventDate = new DateTime(get_field('event_date', false, false));
+                                                echo $eventDate->format('M')
+                                                ?></span>
             <span class="event-summary__day"><?php echo $eventDate->format('d') ?></span>
           </a>
           <div class="event-summary__content">
