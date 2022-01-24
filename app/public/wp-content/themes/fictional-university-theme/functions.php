4a5ac71e69b9faa9ec5776011934c3e78b2d3b5d
Kaja Grabowska <kaja.d.grabowska@gmail.com>
2022-01-24 16:17:00 -0600

    Made login button on homepage work; customized default WordPress login and register screen.

--- app/public/wp-content/themes/fictional-university-theme/functions.php
+++ app/public/wp-content/themes/fictional-university-theme/functions.php
@@ -127,6 +127,29 @@ function noSubsAdminBar()
     }
 }
 
+// Customize Login Screen
+add_filter('login_headerurl', 'ourHeaderUrl');
+
+function ourHeaderUrl() {
+    return esc_url(site_url('/'));
+}
+
+// Function to add CSS to WordPress login page
+add_action('login_enqueue_scripts', 'ourLoginCSS');
+
+function ourLoginCSS() {
+    wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+    wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+    wp_enqueue_style('university_main_styles', get_theme_file_uri('/build/style-index.css'));
+    wp_enqueue_style('university_extra_styles', get_theme_file_uri('/build/index.css'));
+}
+
+add_filter('login_headertitle', 'ourLoginTitle');
+
+function ourLoginTitle() {
+    return get_bloginfo('name');
+}
+
 
 //function for google maps API, can't use because I don't want to enter my billing info
 /* function universityMapKey($api) {
